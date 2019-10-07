ac9f74ec834df063fd694673aae6f3cfee9c135e
Ryan Schmidt <git@ryandesign.com>
2019-10-07 07:18:37 -0500

    Add spaces between literals and identifiers
    This is required in C++11 mode, which newer compilers default to.

--- bf2c.cc
+++ bf2c.cc
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
 	pt = NULL;
 	infname = outfname = invchar = NULL;
 
-	printf("bf2c.cc "VERSION" Copyright (c) 2002-"ENDYEAR" Rene Ladan <r.c.ladan@gmail.com>\n\n"
+	printf("bf2c.cc " VERSION " Copyright (c) 2002-" ENDYEAR " Rene Ladan <r.c.ladan@gmail.com>\n\n"
 		"Optimizing BrainFuck to C compiler.\n\n");
 
 	while ((opt = getopt(argc,argv,"i:o:hs:r:")) != -1) {
